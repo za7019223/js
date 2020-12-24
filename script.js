26268402011ad864cd5cdd1efcfb3c4f5042e768
za7019223 <za7019223@gmail.com>
2020-12-24 13:25:30 +0500

    program1

---
+++ script.js
@@ -0,0 +1,6 @@
+// cecius value from user
+var celcius = prompt("value from user in celcius:");
+// convert celcius to farhenheit
+var fahrenhiet = 9/5*celcius+32;
+// result 
+console.log(fahrenhiet);
